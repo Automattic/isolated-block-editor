91338fa692baf4b5f8e21cc51f4291238ce103c3
Lena Morita <lena@jaguchi.com>
2021-07-27 22:41:48 +0900

    Use webpack 5 for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
 	core: {
-		builder: 'webpack4',
+		builder: 'webpack5',
 	},
 	stories: [ '../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)' ],
 	addons: [ '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-scss' ],
