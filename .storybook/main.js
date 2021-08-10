2aa2daaec8f66dd657feea4ea97543a6f2aeeed3
Lena Morita <lena@jaguchi.com>
2021-08-10 19:08:09 +0900

    Fix invalid glob
    https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#correct-globs-in-mainjs

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
 	core: {
 		builder: 'webpack5',
 	},
-	stories: [ '../(src|stories)/**/*.stories.mdx', '../(src|stories)/**/*.stories.@(js|jsx|ts|tsx)' ],
+	stories: [ '../@(src|stories)/**/*.stories.mdx', '../@(src|stories)/**/*.stories.@(js|jsx|ts|tsx)' ],
 	addons: [ '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-scss' ],
 	webpackFinal: ( config ) => {
 		config.module.rules.push( {
