592f836d774dd4ffc75fe7afdeab143c5c8c6342
John Godley <johng75@gmail.com>
2021-09-07 12:29:10 +0100

    Expand click-outside exceptions
    Chase Gutenberg's ever-changing elements

--- src/components/block-editor-container/click-outside.js
+++ src/components/block-editor-container/click-outside.js
@@ -11,11 +11,27 @@ const ClickOutsideWrapper = withFocusOutside(
 			this.props.onFocus();
 		}
 
+		// Clicks in the media modal or popup components are considered in the editor
+		isInspectorElement( element ) {
+			if ( element.closest( '.components-color-picker' ) ) {
+				return true;
+			}
+
+			if ( element.closest( '.block-editor-block-inspector' ) ) {
+				return true;
+			}
+
+			if ( element.classList.contains( 'media-modal' ) ) {
+				return true;
+			}
+
+			return false;
+		}
+
 		handleFocusOutside( ev ) {
 			const target = ev.relatedTarget || ev.target;
 
-			// Ignore clicks in the media modal - consider it inside the editor
-			if ( target && target.classList.contains( 'media-modal' ) ) {
+			if ( target && this.isInspectorElement( target ) ) {
 				return;
 			}
 
