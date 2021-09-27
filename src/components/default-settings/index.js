2d9d63a8c5d0c25aca541715e20f9cc325f6e980
John Godley <johng75@gmail.com>
2021-09-27 14:36:09 +0100

    Remove fixedToolbar default
    This prevents clients setting a default

--- src/components/default-settings/index.js
+++ src/components/default-settings/index.js
@@ -72,7 +72,6 @@ export default function applyDefaultSettings( settings ) {
 
 			// Default to top toolbar
 			defaultPreferences: {
-				fixedToolbar: true,
 				...( iso?.defaultPreferences ?? {} ),
 			},
 
