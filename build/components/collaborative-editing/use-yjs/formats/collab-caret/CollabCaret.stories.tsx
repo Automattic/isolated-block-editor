9753ea3831af12005111bf1990c0b98a1937b6f8
lamosty <lamos.rasto@gmail.com>
2021-10-08 10:15:47 +0200

    Add built modules

--- build/components/collaborative-editing/use-yjs/formats/collab-caret/CollabCaret.stories.tsx
+++ build/components/collaborative-editing/use-yjs/formats/collab-caret/CollabCaret.stories.tsx
@@ -38,6 +38,29 @@ function CollabCaret( { label, color } ) {
 	);
 }
 
+function CollabCaretMultiLine( { label, color } ) {
+	const { activeFormats } = applyCarets( { formats: [], text: 'foo' }, [ { label, color, start: 21, end: 381 } ] );
+	const { class: additionalClasses, style, ...attributes } = activeFormats[ 0 ].attributes;
+
+	return (
+		<p contentEditable style={ { padding: '8px' } }>
+			Lorem ipsum dolor sit
+			<mark
+				className={ `${ settings.className } ${ additionalClasses }` }
+				style={ cssStringToObject( style ) }
+				{ ...attributes }
+			>
+				mmy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy
+				text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
+				specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+				remaining essentially unchanged. It was popularised
+			</mark>{ ' ' }
+			in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
+			desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+		</p>
+	);
+}
+
 type Props = {
 	label: string;
 	color: string;
@@ -52,3 +75,6 @@ Default.args = {
 	label: 'Peery McPeerson',
 	color: '#C3498D',
 };
+
+export const MultiLine: Story< Props > = CollabCaretMultiLine.bind( {} );
+MultiLine.args = { ...Default.args };
