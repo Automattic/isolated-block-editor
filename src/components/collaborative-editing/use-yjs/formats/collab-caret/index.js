a0d014d92e086b1911cc841d9de1ec602722b88d
Lena Morita <lena@jaguchi.com>
2022-01-24 19:58:33 +0900

    Collab: Prevent emoji from being separated by peer caret

--- src/components/collaborative-editing/use-yjs/formats/collab-caret/index.js
+++ src/components/collaborative-editing/use-yjs/formats/collab-caret/index.js
@@ -30,12 +30,20 @@ export function applyCarets( record, carets = [] ) {
 		const isCollapsed = start === end;
 		const isShifted = isCollapsed && end >= record.text.length;
 
+		// Try to accurately get the `length` of the last character (i.e. grapheme) in case
+		// the last character is an emoji, where "<emoji>".length can be more than 1.
+		// For example, "👩‍👩‍👧‍👦".length === 11. (Intl.Segementer is still unsupported in Firefox)
+		const lastGrapheme = Intl.Segmenter
+			? [ ...new Intl.Segmenter().segment( record.text ) ].at( -1 )?.segment
+			: undefined;
+		const offset = lastGrapheme?.length ?? 1; // fall back to 1 if we can't accurately segment the last grapheme
+
 		if ( isShifted ) {
-			start = record.text.length - 1;
+			start = record.text.length - offset;
 		}
 
 		if ( isCollapsed ) {
-			end = start + 1;
+			end = start + offset;
 		}
 
 		record = applyFormat(
