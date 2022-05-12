9c7fb30b14c07600f7369a368178c816510692ea
Lena Morita <lena@jaguchi.com>
2022-05-12 16:14:05 +0200

    Wrap toolbar test in description

--- src/__tests__/e2e/toolbar-settings.spec.ts
+++ src/__tests__/e2e/toolbar-settings.spec.ts
@@ -3,17 +3,19 @@
  */
 import { test, expect } from '@playwright/test';
 
-test( 'Toolbar settings botton', async ( { page } ) => {
-	await page.goto( '/?path=/story/isolated-block-editor--toolbar-settings' );
-	const editor = page.frameLocator( '#storybook-preview-iframe' );
+test.describe( 'Toolbar settings button', () => {
+	test( 'should open and close popover', async ( { page } ) => {
+		await page.goto( '/?path=/story/isolated-block-editor--toolbar-settings' );
+		const editor = page.frameLocator( '#storybook-preview-iframe' );
 
-	await editor.locator( 'role=button[name="Settings"]' ).click();
+		await editor.locator( 'role=button[name="Settings"]' ).click();
 
-	await expect( editor.locator( 'role=button[name="Document (selected)"]' ) ).toBeVisible();
-	await expect( editor.locator( 'role=button[name="Block"]' ) ).toBeVisible();
+		await expect( editor.locator( 'role=button[name="Document (selected)"]' ) ).toBeVisible();
+		await expect( editor.locator( 'role=button[name="Block"]' ) ).toBeVisible();
 
-	await editor.locator( 'role=button[name="Close settings"]' ).click();
+		await editor.locator( 'role=button[name="Close settings"]' ).click();
 
-	await expect( editor.locator( 'role=button[name="Document (selected)"]' ) ).not.toBeVisible();
-	await expect( editor.locator( 'role=button[name="Block"]' ) ).not.toBeVisible();
+		await expect( editor.locator( 'role=button[name="Document (selected)"]' ) ).not.toBeVisible();
+		await expect( editor.locator( 'role=button[name="Block"]' ) ).not.toBeVisible();
+	} );
 } );
