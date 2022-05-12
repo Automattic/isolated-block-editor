5646ee6966abf80c0da5a242ee4d5b97793a44a0
Camilla Krag Jensen <naxoc@users.noreply.github.com>
2022-05-12 15:56:26 +0200

    Move test file

---
+++ src/__tests__/e2e/toolbar-settings.spec.ts
@@ -0,0 +1,19 @@
+/**
+ * External dependencies
+ */
+import { test, expect } from '@playwright/test';
+
+test( 'Toolbar settings botton', async ( { page } ) => {
+	await page.goto( '/?path=/story/isolated-block-editor--toolbar-settings' );
+	const editor = page.frameLocator( '#storybook-preview-iframe' );
+
+	await editor.locator( 'role=button[name="Settings"]' ).click();
+
+	await expect( editor.locator( 'role=button[name="Document (selected)"]' ) ).toBeVisible();
+	await expect( editor.locator( 'role=button[name="Block"]' ) ).toBeVisible();
+
+	await editor.locator( 'role=button[name="Close settings"]' ).click();
+
+	await expect( editor.locator( 'role=button[name="Document (selected)"]' ) ).not.toBeVisible();
+	await expect( editor.locator( 'role=button[name="Block"]' ) ).not.toBeVisible();
+} );
