de7882ecff917a2d03f2fea1595aabcab640685f
Matías Surdi <matias.emanuel.surdi@automattic.om>
2022-06-07 14:16:15 +0200

    Remove unused history prop from type definition

--- src/index.js
+++ src/index.js
@@ -83,7 +83,6 @@ import './style.scss';
  * @property {string[]} [disallowEmbed] - List of embed names to remove
  * @property {object[]} [customStores] - Array of custom stores
  * @property {boolean} [footer] - Show footer component
- * @property {history} [history] - Enable hisotry undo/redo functionality
  * @property {ToolbarSettings} [toolbar] - Toolbar settings
  * @property {MoreMenuSettings|false} [moreMenu] - More menu settings, or false to disable
  * @property {{title: string, url: string}[]} [linkMenu] - Link menu settings
