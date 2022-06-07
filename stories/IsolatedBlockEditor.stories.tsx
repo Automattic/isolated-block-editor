44b3c26e02af0723f3499ef2768f2624b5ccc5e4
Matías Surdi <matias.emanuel.surdi@automattic.om>
2022-06-07 14:28:34 +0200

    Remove unnecessary blocks registration

--- stories/IsolatedBlockEditor.stories.tsx
+++ stories/IsolatedBlockEditor.stories.tsx
@@ -22,10 +22,6 @@ export const Controlled = ( { onInput, onChange, onUndo, onRedo } ) => {
 	const [ blocks, setBlocks ] = useState( [] );
 	const [ log, setLog ] = useState( [] );
 
-	useEffect( () => {
-		registerCoreBlocks();
-	}, [] );
-
 	const handleOnInput = ( newBlocks ) => {
 		onInput( newBlocks );
 		setBlocks( newBlocks );
