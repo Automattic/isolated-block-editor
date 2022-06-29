677efaeb8763ae5662c34ddd48f062a4327c6ba1
John Godley <johng75@gmail.com>
2022-06-29 18:00:20 +0100

    Update local interface store

--- src/components/with-registry-provider/interface-store/index.js
+++ src/components/with-registry-provider/interface-store/index.js
@@ -3,16 +3,17 @@
  */
 import * as actions from './actions';
 import * as selectors from './selectors';
+import reducer from './reducer';
 
 /**
- * Store definition for the edit post namespace.
+ * Store definition for the interface namespace.
  *
  * @see https://github.com/WordPress/gutenberg/blob/HEAD/packages/data/README.md#createReduxStore
  *
  * @type {Object}
  */
 export default {
-	reducer: () => {},
+	reducer,
 	actions,
 	selectors,
 };
