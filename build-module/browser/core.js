c5b72dc9ed64009377d1eccfea5d30f0cc6954d5
nickthetj <nicholastjandra1@gmail.com>
2022-07-28 17:31:56 +0800

    Setting up storybook locally

---
+++ build-module/browser/core.js
@@ -0,0 +1,5 @@
+/**
+ * Internal dependencies
+ */
+import './core.scss';
+//# sourceMappingURL=core.js.map
--- build/components/collaborative-editing/use-yjs/formats/collab-caret/CollabCaret.stories.tsx
+++ build/components/collaborative-editing/use-yjs/formats/collab-caret/CollabCaret.stories.tsx
@@ -34,7 +34,7 @@ function CollabCaret( { label, color } ) {
 
 	return (
 		<p contentEditable style={ { padding: '8px' } }>
-			Lorem ipsum dolor sit
+			 ipsum dolor sit
 			<mark
 				className={ `${ settings.className } ${ additionalClasses }` }
 				style={ cssStringToObject( style ) }
@@ -53,19 +53,19 @@ function CollabCaretMultiLine( { label, color } ) {
 
 	return (
 		<p contentEditable style={ { padding: '8px' } }>
-			Lorem ipsum dolor sit
+			 ipsum dolor sit
 			<mark
 				className={ `${ settings.className } ${ additionalClasses }` }
 				style={ cssStringToObject( style ) }
 				{ ...attributes }
 			>
-				mmy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy
+				mmy text of the printing and typesetting industry.  Ipsum has been the industrys standard dummy
 				text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
 				specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
 				remaining essentially unchanged. It was popularised
 			</mark>{ ' ' }
-			in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
-			desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+			in the 1960s with the release of Letraset sheets containing  Ipsum passages, and more recently with
+			desktop publishing software like Aldus PageMaker including versions of  Ipsum.
 		</p>
 	);
 }
