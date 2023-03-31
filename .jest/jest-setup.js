186d545089dfd9f3a5357c1933be998ab6cfff2a
John Godley <johng75@gmail.com>
2023-03-31 14:26:57 +0100

    Fix jest build error

--- .jest/jest-setup.js
+++ .jest/jest-setup.js
@@ -2,6 +2,13 @@ import '@testing-library/jest-dom';
 
 // Polyfill for window.requestIdleCallback, used in `@wordpress/priority-queue`
 import '@shopify/polyfills/idle-callback.jest';
+import crypto from 'crypto'
+
+Object.defineProperty( global, 'crypto', {
+	value: {
+		getRandomValues: ( arr ) => crypto.randomBytes( arr.length )
+	}
+} );
 
 global.ResizeObserver = require( 'resize-observer-polyfill' );
 
