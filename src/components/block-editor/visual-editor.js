fa8e5f22e8b1bb25d63867b1fd804c748cc73146
John Godley <johng75@gmail.com>
2023-10-31 09:54:29 +0000

    ‘Fix’ for 16.9

--- src/components/block-editor/visual-editor.js
+++ src/components/block-editor/visual-editor.js
@@ -38,7 +38,7 @@ import FooterSlot from '../footer-slot';
 
 export const { lock, unlock } =
 	__dangerousOptInToUnstableAPIsOnlyForCoreModules(
-		'I know using unstable features means my plugin or theme will inevitably break on the next WordPress release.',
+		'I know using unstable features means my theme or plugin will inevitably break in the next version of WordPress.',
 		'@wordpress/edit-post'
 	);
 
