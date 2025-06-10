b2ec1c955b3145f38632a0cfc2b2e8fff0248b24
Jarda Snajdr <jsnajdr@gmail.com>
2025-06-10 11:03:01 +0200

    Add TextDecoder polyfills

--- .jest/jest-setup.js
+++ .jest/jest-setup.js
@@ -3,6 +3,8 @@ import '@testing-library/jest-dom';
 // Polyfill for window.requestIdleCallback, used in `@wordpress/priority-queue`
 import '@shopify/polyfills/idle-callback.jest';
 import crypto from 'crypto'
+import { TextDecoder, TextEncoder } from 'node:util';
+import ResizeObserver from 'resize-observer-polyfill';
 
 Object.defineProperty( global, 'crypto', {
 	value: {
@@ -10,7 +12,9 @@ Object.defineProperty( global, 'crypto', {
 	}
 } );
 
-global.ResizeObserver = require( 'resize-observer-polyfill' );
+global.ResizeObserver = ResizeObserver;
+global.TextDecoder = TextDecoder;
+global.TextEncoder = TextEncoder;
 
 jest.mock( '@wordpress/compose', () => {
 	return {
